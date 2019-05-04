3fe758cda82cbfe916ac4423c5dd8bb169833ba6
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 21:58:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,4 +22,6 @@ end
 def find_the_cheese(contains_cheddar)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  contains_cheddar.select do |cheese|
+    
 end
