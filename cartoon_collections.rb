27359c55c138bc766f0efe3c5e30ce89a513d11c
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 21:45:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,7 @@ def summon_captain_planet(veggies)# code an argument here
   # Your code here
   veggies.collect do |veggie|
     veggie.upcase << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
